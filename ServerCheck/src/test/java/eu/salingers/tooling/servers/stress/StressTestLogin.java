1c32aa4ed639c43f3d935f99c949739aed79b0a5
orestis.salinger <orestis@salingers.eu>
2015-12-12 14:22:43 +0100

    Refactoring CSV-reading and Testing.

---
+++ ServerCheck/src/test/java/eu/salingers/tooling/servers/stress/StressTestLogin.java
@@ -0,0 +1,30 @@
+package eu.salingers.tooling.servers.stress;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import eu.salingers.tooling.servers.data.file.TestServers;
+import eu.salingers.tooling.servers.model.servers.Server;
+
+public class StressTestLogin extends TestServers {
+
+  @Test
+  public void handleRequests_loginStress4Staging_logInGrantedOnAllLoginAttempts() throws Exception {
+    List<Server> servers = createServerListFromFile(FILENAME_CREDENTIALS_STRESS_STAGING);
+
+    handleRequestsForServers(servers);
+
+    servers.stream().forEach(e -> {
+      final String responseHtml = e.getResponseHtml();
+//      System.out.println("Correct Login"+e.getUrl()+" \n" + e.getUsername() +":"+ e.getPassword());
+      assertThat(responseHtml, containsString(PROOF_LOGGED_IN));
+    });
+  }
+
+
+}
